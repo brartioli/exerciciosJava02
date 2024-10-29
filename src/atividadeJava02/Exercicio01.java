5973815a6b1270d5143ef4f2be8cda7fcee5a5d8
brartioli <brunaartioli.ba@gmail.com>
2024-10-29 11:19:33 -0300

    primeiro commit

---
+++ src/atividadeJava02/Exercicio01.java
@@ -0,0 +1,37 @@
+package atividadeJava02;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		int A, B, C, soma;
+		
+		System.out.printf("Digite o número A:");
+		A = leia.nextInt();
+		System.out.printf("Digite o número B:");
+		B = leia.nextInt();
+		System.out.printf("Digite o número C:");
+		C = leia.nextInt();
+	
+		soma = A + B;
+		
+		if (soma > C) {
+			System.out.println(A + " +" + B + " = " +soma + " > " + C);
+			System.out.println("A soma de A + B é maior do que C");
+		}
+		else if(soma < C) {
+			System.out.println(A + " + " + B + " = " + soma + " < " + C);
+			System.out.println("A soma de A + B é menor do que C");
+		}
+		else {
+			System.out.println(A + " + " + B + " = " + soma + " = " + C);
+			System.out.println("A soma de A + B é igual a C");
+			
+		}
+		leia.close();
+	}
+
+}
