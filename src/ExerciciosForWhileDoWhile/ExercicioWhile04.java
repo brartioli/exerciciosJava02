88ba07f50875c27956c9375a2aab681950b9cdd2
brartioli <brunaartioli.ba@gmail.com>
2024-10-30 17:29:21 -0300

    sexto commit

---
+++ src/ExerciciosForWhileDoWhile/ExercicioWhile04.java
@@ -0,0 +1,13 @@
+package ExerciciosForWhileDoWhile;
+
+import java.util.Scanner;
+
+public class ExercicioWhile04 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
