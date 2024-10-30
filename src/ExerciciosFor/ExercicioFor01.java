13817025f05fc9a7b9c41ea93e83708f66a30492
brartioli <brunaartioli.ba@gmail.com>
2024-10-30 14:38:54 -0300

    quarto commit

---
+++ src/ExerciciosFor/ExercicioFor01.java
@@ -0,0 +1,30 @@
+package ExerciciosFor;
+
+import java.util.Scanner;
+
+public class ExercicioFor01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B;
+		
+		System.out.printf("Digite o primeiro número do intervalo: ");
+		A = sc.nextInt();
+		System.out.printf("Digite o último número do intervalo: ");
+		B = sc.nextInt();
+		
+		if(A>B) {
+			System.out.println("Intervalo inválido!");
+		}else
+		{
+			for(int i =A; i<B;i++) {
+				if(i % 3 == 00 && i % 5 == 0) {
+					System.out.println(i + " é múltiplo de 3 e 5");
+				}
+			}
+		}
+		sc.close();
+	}
+
+}
---
+++ src/ExerciciosFor/ExercicioFor02.java
@@ -0,0 +1,38 @@
+package ExerciciosFor;
+
+import java.util.Scanner;
+
+public class ExercicioFor02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+		int contPar, contIm;
+		
+		System.out.printf("Digite o 1° número: ");
+		n1 = sc.nextInt();
+		System.out.printf("Digite o 2° número: ");
+		n2= sc.nextInt();
+		System.out.printf("Digite o 3° número: ");
+		n3 = sc.nextInt();
+		System.out.printf("Digite o 4° número: ");
+		n4 = sc.nextInt();
+		System.out.printf("Digite o 5° número: ");
+		n5 = sc.nextInt();
+		System.out.printf("Digite o 6° número: ");
+		n6 = sc.nextInt();
+		System.out.printf("Digite o 7° número: ");
+		n7 = sc.nextInt();
+		System.out.printf("Digite o 8° número: ");
+		n8 = sc.nextInt();
+		System.out.printf("Digite o 9° número: ");
+		n9 = sc.nextInt();
+		System.out.printf("Digite o 10° número: ");
+		n10 = sc.nextInt();
+		
+		
+		
+	}
+
+}
